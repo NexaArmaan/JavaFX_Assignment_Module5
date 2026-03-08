271e4c94a16319224bc77ebc3427e1763c91dadd
Armaan Arora <armaanarora880@gmail.com>
2026-03-07 22:50:46 -0500

    Menu Bar created

--- src/main/java/org/example/javafx_assignment_module5/HelloApplication.java
+++ src/main/java/org/example/javafx_assignment_module5/HelloApplication.java
@@ -3,7 +3,9 @@ package org.example.javafx_assignment_module5;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
@@ -23,13 +25,51 @@ public class HelloApplication extends Application {
         VBox leftSide = createLeftSide();
         VBox rightSide = createRightSide;
         StackPane centerPanel = createCenterPanel();
-        Pane bottonBar = createBottomBar();
+        Pane bottomBar = createBottomBar();
 
         root.setTop(menuBar);
+        root.setLeft(leftSide);
+        root.setRight(rightSide);
+        root.setCenter(centerPanel);
+        root.setBottom(bottomBar);
 
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("JavaFX_Assignment");
         stage.setScene(scene);
         stage.show();
     }
+
+    private MenuBar createMenuBar(){
+        MenuBar menuBar = new MenuBar();
+
+        Menu fileMenu = new Menu("File");
+        Menu newItem = new Menu("New");
+        Menu exitMenu = new Menu("Exit");
+        fileMenu.getItems().addAll(newItem,new SeparatorMenuItem(), exitMenu);
+
+        Menu editMenu = new Menu("Edit");
+        Menu clearMenu = new Menu("Clear Fields");
+        Menu deleteItem = new Menu("Delete Selected Items");
+        editMenu.getItems().addAll(clearMenu,deleteItem);
+
+        Menu themeMenu = new Menu("Theme");
+        Menu defaultMenu = new Menu("Default Theme");
+        Menu colorMenu = new Menu("Colored Theme");
+        themeMenu.getItems().addAll(defaultMenu,colorMenu);
+
+        Menu helpMenu = new Menu("Help");
+        Menu aboutMenu = new Menu("About");
+        helpMenu.getItems().addAll(aboutMenu);
+
+        newItem.setOnAction(e -> clearFields());
+        clearMenu.setOnAction(e -> clearFields());
+        exitMenu.setOnAction(e -> ((Stage) menuBar.getScene().getWindow()).close());
+        deleteItem.setOnAction(e-> deleteSelectedFields());
+        defaultMenu.setOnAction(e-> infoDisplay("Theme","Default theme is applied"));
+        aboutMenu.setOnAction(e-> infoDisplay("About","JavaFX is good"));
+
+        menuBar.getMenus().addAll(fileMenu,editMenu,themeMenu,helpMenu);
+        return menuBar;
+    }
+
 }
